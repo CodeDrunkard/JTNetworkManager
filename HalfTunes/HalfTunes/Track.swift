0b0a3c637da8eb25a0afcee2984729a008167f5f
JT Ma <ma.jiangtao.86@gmail.com>
2017-08-30 16:24:30 +0800

    Add: download example

---
+++ HalfTunes/HalfTunes/Track.swift
@@ -0,0 +1,22 @@
+//
+//  Track.swift
+//  HalfTunes
+//
+//  Created by Ken Toh on 13/7/15.
+//  Copyright (c) 2015 Ken Toh. All rights reserved.
+//
+
+import Foundation
+
+class Track {
+    var name: String?
+    var artist: String?
+    var previewUrl: String?
+    var localUrl: URL?
+    
+    init(name: String?, artist: String?, previewUrl: String?) {
+        self.name = name
+        self.artist = artist
+        self.previewUrl = previewUrl
+    }
+}
